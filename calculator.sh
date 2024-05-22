8513cbf17085e9531067fccc929c63e4b23ed82f
jkato23 <163766524+jkato23@users.noreply.github.com>
2024-05-22 01:06:57 -0700

    Update calculator.sh
    Updated the code to use case statements instead of if statements.

--- calculator.sh
+++ calculator.sh
@@ -1,33 +1,26 @@
 while true
 do
-        echo "1. Add"
-        echo "2. Subtract"
-        echo "3. Multiply"
-        echo "4. Divide"
-        echo "5. Quit"
-        read -p "Choose an operation to perform: " choice
-        if [ $choice -eq 1 ]
-        then
-                read -p "Enter the first number: " num1
-                read -p "Enter the second number:" num2
-                echo "Answer=$(( num1 + num2 ))"
-        elif [ $choice -eq 2 ]
-        then
-                read -p "Enter the first number: " num1
-                read -p "Enter the second number: " num2
-                echo "Answer=$(( num1 - num2 ))"
-        elif [ $choice -eq 3 ]
-        then
-                read -p "Enter the first number: " num1
-                read -p "Enter the second number: " num2
-                echo "Answer=$(( num1 * num2 ))"
-        elif [ $choice -eq 4 ]
-        then
-                read -p "Enter the first number: " num1
-                read -p "Enter the second number: " num2
-                echo "Answer=$(( num1 / num2 ))"
-        elif [ $choice -eq 5 ]
-        then
-                break
-        fi
+  echo "1. Add"
+  echo "2. Subtract"
+  echo "3. Multiply"
+  echo "4. Divide"
+  echo "5. Quit"
+
+  read -p "Enter your choice: " choice
+
+  case $choice in
+        1) read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 + $number2 )) ;;
+        2) read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 - $number2 )) ;;
+        3) read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 * $number2 )) ;;
+        4) read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 / $number2 )) ;;
+        5) break ;;
+ esac
 done
