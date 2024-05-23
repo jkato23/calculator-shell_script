fffb3cb51cf03726dfa4f472d0f38f851c590229
jkato23 <163766524+jkato23@users.noreply.github.com>
2024-05-22 21:33:10 -0700

    Update calculator.sh
    Added a function for reused code, an option to find the mean of two numbers, and a blank line after returning an answer.

--- calculator.sh
+++ calculator.sh
@@ -1,26 +1,35 @@
+function get_numbers(){
+  read -p "Enter Number1: " number1
+  read -p "Enter Number2: " number2
+}
+
 while true
 do
   echo "1. Add"
   echo "2. Subtract"
   echo "3. Multiply"
   echo "4. Divide"
-  echo "5. Quit"
+  echo "5. Mean"
+  echo "6. Quit"
 
   read -p "Enter your choice: " choice
 
   case $choice in
-        1) read -p "Enter Number1: " number1
-        read -p "Enter Number2: " number2
-        echo Answer=$(( $number1 + $number2 )) ;;
-        2) read -p "Enter Number1: " number1
-        read -p "Enter Number2: " number2
-        echo Answer=$(( $number1 - $number2 )) ;;
-        3) read -p "Enter Number1: " number1
-        read -p "Enter Number2: " number2
-        echo Answer=$(( $number1 * $number2 )) ;;
-        4) read -p "Enter Number1: " number1
-        read -p "Enter Number2: " number2
-        echo Answer=$(( $number1 / $number2 )) ;;
-        5) break ;;
+        1) get_numbers
+        echo Answer=$(( $number1 + $number2 ))
+        echo "" ;;
+        2) get_numbers
+        echo Answer=$(( $number1 - $number2 ))
+        echo "" ;;
+        3) get_numbers
+        echo Answer=$(( $number1 * $number2 ))
+        echo "" ;;
+        4) get_numbers
+        echo Answer=$(( $number1 / $number2 ))
+        echo "" ;;
+        5) get_numbers
+        echo Answer=$((($number1 + $number2) / 2 ))
+        echo "" ;;
+        6) break ;;
  esac
 done
